29612a3751ddbb2d5fe1b8f0adad718df1c24f87
Paul  Stanley Gebeline <psgebeli@go.olemiss.edu>
2023-09-16 13:25:47 -0500

    continue working on working.py

--- problems/problemset7/working/working.py
+++ problems/problemset7/working/working.py
@@ -51,11 +51,58 @@ import re
 import sys 
 
 def main():
-    ...
+    
+    time_range = input("When do you work? ")
+    print(convert(time_range))
+
 
 def convert(s):
 
-    if matches := re.search(r"^(\d\d?)(:\d\d)?\s(AM|PM)\sto(\d\d?)(:\d\d)?\s(AM|PM)$", s):
-        start_hour, start_minutes, start_meridiem = matches.group(1), matches.group(2).replace(':', ''), matches.group(3)
-        stop_hour, stop_minutes, stop_meridiem = matches.group(4), matches.group(5).replace(':', ''), matches.group(6)
+    if matches := re.search(r"(\d\d?)(:\d\d)?\s(AM|PM)\sto\s(\d\d?)(:\d\d)?\s(AM|PM)", s):
+
+
+        start_hour, start_meridiem = int(matches.group(1)), matches.group(3)
+        stop_hour, stop_meridiem = int(matches.group(4)), matches.group(6)
+
+        start_minutes = int(matches.group(2).replace(':', '')) if matches.group(2) else 0
+        stop_minutes = int(matches.group(5).replace(':', '')) if matches.group(5) else 0
+
+        start_decimal = start_hour + start_minutes / 60 if start_minutes else start_hour
+        stop_decimal = stop_hour + stop_minutes / 60 if stop_minutes else stop_hour
+
+        match start_meridiem:
+            case 'AM':
+                if 12.0 <= start_decimal < 13.0:
+                    start24 = start_decimal - 12.0
+                else:
+                    start24 = start_decimal
+            case 'PM':
+                if 1.0 <= start_decimal < 12.0:
+                    start24 = start_decimal + 12.0
+                else:
+                    start24 = start_decimal
         
+        match stop_meridiem:
+            case 'AM':
+                if 12.0 <= stop_decimal < 13.0:
+                    stop24 = stop_decimal - 12.0
+                else:
+                    stop24 = stop_decimal
+            case 'PM':
+                if 1.0 <= stop_decimal < 12.0:
+                    stop24 = stop_decimal + 12.0
+                else:
+                    stop24 = stop_decimal
+
+
+        # Format the time with a colon between hours and minutes
+        start24_str = f'{int(start24):02}:{int((start24 % 1) * 60):02}'
+        stop24_str = f'{int(stop24):02}:{int((stop24 % 1) * 60):02}'
+
+        return f'{start24_str} to {stop24_str}'
+    else:
+        raise ValueError("invalid input")
+        
+        
+if __name__ == '__main__':
+    main()
